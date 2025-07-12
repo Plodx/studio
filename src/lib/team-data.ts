b233318d50bde404cff15c03fc4f78f377431bd9
Sebastian <sebasnoda@gmail.com>
2025-07-12 23:37:13 +0000

    Modificame la lista de los equipos fuertes por estos: Chelsea, Mancheste

--- src/lib/team-data.ts
+++ src/lib/team-data.ts
@@ -1,5 +1,6 @@
+
 export const strongTeamsList: string[] = [
-  "Chelsea", "Manchester United", "Real Madrid", "Barcelona", "Inter de Milan", "Milan", "Juventus", "England", "Francia", "Brasil", "España", "Holanda", "Italia", "Argentina 25", "Brasil 25", "Chelsea 25", "Inglaterra 25", "Bayern Munich 25", "Barcelona 25", "Inter de Milan 25", "Francia 25", "Alemania 25", "Juventus 25", "Liverpool 25", "Man City 25", "Man. Und. 25", "PSG 25", "Portugal 25", "Real Madrid 25", "España 25", "Italia 25", "Atletico Madrid 25", "Aston Villa 25", "Arsenal 25", "NewCastle 25", "Tottenham 25", "COMODIN A"
+  "Chelsea", "Manchester United", "Real Madrid", "Barcelona", "Inter de Milan", "Milan", "Juventus", "England", "Francia", "Brasil", "España", "Holanda", "Italia", "Argentina 25", "Brasil 25", "Inglaterra 25", "Bayern Munich 25", "Barcelona 25", "Inter 25", "Francia 25", "Alemania 25", "Liverpool 25", "Man City 25", "PSG 25", "Portugal 25", "Real Madrid 25", "España 25", "Italia 25", "Atletico Madrid 25", "Colombia 25", "Arsenal 25", "Holanda 25", "Bayer Leverkusen 25", "COMODIN A"
 ];
 
 export const weakTeamsList: string[] = [
