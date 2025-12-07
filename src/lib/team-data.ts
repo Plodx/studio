52ae849b8496c20148def131115cdf4004815880
Sebastian <sebasnoda@gmail.com>
2025-12-07 15:29:24 +0000

    se saco a celtic de fuerte y se mando a debil

--- src/lib/team-data.ts
+++ src/lib/team-data.ts
@@ -8,9 +8,9 @@ export const newWeakTeamsList: string[] = [
 ];
 
 export const legacyStrongTeamsList: string[] = [
-  "Argentina", "Arsenal", "Barcelona", "Bayern Munchen", "Brasil", "Celtic", "Chelsea", "Comodín A", "España", "Francia", "Alemania", "Holanda", "Inglaterra", "Inter", "Italia", "Juventus", "Liverpool", "Man United", "Milan", "Newcastle", "Portugal", "Real Madrid", "República Checa", "Roma", "Suecia", "Tottenham", "Valencia", "Villareal"
+  "Argentina", "Arsenal", "Barcelona", "Bayern Munchen", "Brasil", "Chelsea", "Comodín A", "España", "Francia", "Alemania", "Holanda", "Inglaterra", "Inter", "Italia", "Juventus", "Liverpool", "Man United", "Milan", "Newcastle", "Portugal", "Real Madrid", "República Checa", "Roma", "Suecia", "Tottenham", "Valencia", "Villareal"
 ];
 
 export const legacyWeakTeamsList: string[] = [
-  "Ajax", "Atletico Madrid", "Benfica", "Belgica", "Betis", "Boca", "Camerun", "Chievo Verona", "Comodín B", "Costa de Marfil", "Croacia", "Dinamarca", "Everton", "Fiorentina", "Lazio", "Lyon", "Man City", "Marseille", "Monaco", "Palermo", "Porto", "PSV", "River", "Sampdoria", "Sevilla", "Siena", "Sporting Lisboa", "Turquia", "Ucrania", "Udinese", "West Ham", "Zaragoza", "Aston Villa", "Middlebrook", "Charlton", "Fulham", "Tesside", "Pompy", "Saint-Étienne", "Olympiacos", "Irlanda", "Uruguay"
+  "Ajax", "Celtic", "Atletico Madrid", "Benfica", "Belgica", "Betis", "Boca", "Camerun", "Chievo Verona", "Comodín B", "Costa de Marfil", "Croacia", "Dinamarca", "Everton", "Fiorentina", "Lazio", "Lyon", "Man City", "Marseille", "Monaco", "Palermo", "Porto", "PSV", "River", "Sampdoria", "Sevilla", "Siena", "Sporting Lisboa", "Turquia", "Ucrania", "Udinese", "West Ham", "Zaragoza", "Aston Villa", "Middlebrook", "Charlton", "Fulham", "Tesside", "Pompy", "Saint-Étienne", "Olympiacos", "Irlanda", "Uruguay"
 ];
