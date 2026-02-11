9ab1bca4257c21b788d89121ecd0941a99ac3e26
Sebastian <sebasnoda@gmail.com>
2026-02-11 23:54:57 +0000

    Cambio de equipos en NEWList

--- src/lib/team-data.ts
+++ src/lib/team-data.ts
@@ -1,10 +1,10 @@
 
 export const newStrongTeamsList: string[] = [
-  "Chelsea", "Manchester United", "Real Madrid", "Barcelona", "Inter de Milan", "Milan", "Juventus", "England", "Francia", "Brasil", "España", "Holanda", "Italia", "Argentina 25", "Brasil 25", "Inglaterra 25", "Bayern Munich 25", "Barcelona 25", "Inter 25", "Francia 25", "Alemania 25", "Liverpool 25", "Man City 25", "PSG 25", "Portugal 25", "Real Madrid 25", "España 25", "Italia 25", "Atletico Madrid 25", "Colombia 25", "Arsenal 25", "Holanda 25", "Bayer Leverkusen 25", "Belgica 25", "COMODIN A"
+  "Argentina", "Arsenal", "Barcelona", "Bayern Munchen", "Brasil", "Chelsea", "Comodín A", "España", "Francia", "Alemania", "Holanda", "Inglaterra", "Inter", "Italia", "Juventus", "Liverpool", "Man United", "Milan", "Bayern Munchen 15", "Portugal", "Real Madrid", "República Checa", "Roma", "Suecia", "Real Madrid 15", "PSG 15", "Barcelona 15"
 ];
 
 export const newWeakTeamsList: string[] = [
-  "Monaco", "Lyon", "Totenham", "Udinese", "Marsellie", "Benfica", "Atletico Madrid", "Turkey", "Boca", "River", "Liverpool", "Arsenal", "Bayern Munich", "Argentina", "Zaragoza", "Rep Checa", "Roma", "Portugal", "Valencia", "Newcastle", "Villarreal", "Alemania", "Suecia", "Ajax", "Celtic", "Leipzig 25", "Noruega 25", "Benfica 25", "Chelsea 25", "Milan 25", "Napoli 25", "Man United 25", "COMODIN B", "Al-Nassr 25", "Celtic 25", "Galatasaray 25", "Inter Miami 25", "Newcastle 25", "Juventus 25", "River 25", "Roma 25", "Borussia Dortmun 25", "Aston Villa 25", "Atlhetic Bilbao 25", "Tottenham 25"
+  "Ajax", "Celtic", "Atletico Madrid", "Benfica", "Belgica", "Betis", "Boca", "Villareal", "Chievo Verona", "Comodín B", "Costa de Marfil", "Croacia", "Dinamarca", "Everton", "Fiorentina", "Lazio", "Lyon", "Man City", "Marseille", "Monaco", "Palermo", "Porto", "PSV", "River", "Newcastle", "Sampdoria", "Sevilla", "Tottenham", "Siena", "Sporting Lisboa", "Turquia", "Ucrania", "Udinese", "West Ham", "Zaragoza", "Aston Villa", "Middlebrook", "Charlton", "Fulham", "Tesside", "Valencia", "Saint-Étienne", "Olympiacos", "Irlanda", "Uruguay"
 ];
 
 export const legacyStrongTeamsList: string[] = [
